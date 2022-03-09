be530c376d88fb744b4cfb40757c7a92e55072b9
LautaroFullone <lautarofullone@gmail.com>
2022-03-08 23:01:46 -0300

    Creating packages and mini User implementation

---
+++ src/main/java/com/utn/tesis/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.utn.tesis.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.utn.tesis.model.User;
+import com.utn.tesis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User u = userService.addUser(user);
+        return ResponseEntity.ok().body(u);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> userList = userService.getUsers();
+
+        if(userList.isEmpty())
+            return ResponseEntity.noContent().build();
+        else
+            return ResponseEntity.status(HttpStatus.OK)
+                    .header("X-Total-Elements", Integer.toString(userList.size()))
+                    .body(userList);
+
+    }
+}
---
+++ src/main/java/com/utn/tesis/model/User.java
@@ -0,0 +1,25 @@
+package com.utn.tesis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name ="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+    private String email;
+    private String password;
+}
