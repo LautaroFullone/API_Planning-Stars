ab2831b6f0c12d00d7ffdbcc4ecc55da5156139d
LautaroFullone <lautarofullone@gmail.com>
2022-03-15 22:28:57 -0300

    Creating packages and mini User implementation

--- src/main/java/com/utn/tesis/model/User.java
+++ src/main/java/com/utn/tesis/model/User.java
@@ -17,7 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
 
     private String name;
     private String email;
