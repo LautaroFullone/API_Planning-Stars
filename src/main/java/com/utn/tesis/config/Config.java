e67074b0bc5a0b313c34889eb3c59f95b67294af
Nicolas Bertuccio <57155671+NicolasJbo@users.noreply.github.com>
2022-03-29 18:35:46 -0300

    Removing  useless Imports

--- src/main/java/com/utn/tesis/config/Config.java
+++ src/main/java/com/utn/tesis/config/Config.java
@@ -3,8 +3,7 @@ package com.utn.tesis.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class Config {
