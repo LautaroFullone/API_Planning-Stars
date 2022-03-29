4d1a361b584b5ceb1f484f60a330c74a19d4049f
Nicolas Bertuccio <57155671+NicolasJbo@users.noreply.github.com>
2022-03-29 18:32:58 -0300

    JWT Token Applied

---
+++ src/main/java/com/utn/tesis/config/WebSecurity.java
@@ -0,0 +1,26 @@
+package com.utn.tesis.config;
+
+import com.utn.tesis.filter.JWTAuthorizationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/user/register").permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
--- src/main/java/com/utn/tesis/controller/UserController.java
+++ src/main/java/com/utn/tesis/controller/UserController.java
@@ -1,23 +1,38 @@
 package com.utn.tesis.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.utn.tesis.exception.types.EmailExistException;
+import com.utn.tesis.model.LoginResponseDto;
 import com.utn.tesis.model.User;
 import com.utn.tesis.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.utn.tesis.util.UTIL_CONSTANT.JWT_SECRET;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService,ObjectMapper objectMapper) {
         this.userService = userService;
+        this.objectMapper=objectMapper;
     }
 
     @PostMapping("/register")
@@ -26,10 +41,12 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(usr);
     }
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody User userLogging){
+    public ResponseEntity login(@RequestBody User userLogging) throws JsonProcessingException {
         User finalUser = userService.login(userLogging);
-        return ResponseEntity.status(HttpStatus.OK).body(finalUser);
+            return ResponseEntity.ok(LoginResponseDto.builder().token(this.generateToken(finalUser)).build());
     }
+
+
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         List<User> userList = userService.getUsers();
@@ -41,6 +58,23 @@ public class UserController {
                     .header("X-Total-Elements", Integer.toString(userList.size()))
                     .body(userList);
 
+    }
+
+    public String generateToken(User user) throws JsonProcessingException {
+        String role ="USER";
+        //parte de spring security que permite la utilizacion de roles jwt
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(role);
+
+        String token = Jwts.builder()
+                .setId("JWT")
+                .setSubject(user.getEmail())
+                .claim("user",objectMapper.writeValueAsString(user))
+                .claim("authorities",grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 180000000 ))
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET.getBytes()).compact();
+
+        return token;
     }
 
 }
