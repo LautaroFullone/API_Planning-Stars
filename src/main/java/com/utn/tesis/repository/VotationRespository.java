2a6e4aa6aca5cf4bcfe197025d5f5f0d52584347
Nicolas Bertuccio <57155671+NicolasJbo@users.noreply.github.com>
2022-07-06 18:51:55 -0300

    Creating Votation service and method in PartyController

--- src/main/java/com/utn/tesis/repository/VotationRespository.java
+++ src/main/java/com/utn/tesis/repository/VotationRespository.java
@@ -3,5 +3,8 @@ package com.utn.tesis.repository;
 import com.utn.tesis.model.Votation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VotationRespository extends JpaRepository<Votation,Integer> {
+    List<Votation> findByUserStoryIdIn(List<Integer> idUs);
 }
