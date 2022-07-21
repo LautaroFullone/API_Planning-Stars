229346c55e1a79644aabe526c9a5542e154780f9
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-07-21 16:00:19 -0300

    exception change 2

--- src/main/java/com/utn/tesis/exception/types/EmailExistException.java
+++ src/main/java/com/utn/tesis/exception/types/EmailExistException.java
@@ -2,6 +2,6 @@ package com.utn.tesis.exception.types;
 
 public class EmailExistException extends RuntimeException  {
     public EmailExistException() {
-        super("EMAIL in use.");
+        super("Entry email in use");
     }
 }
