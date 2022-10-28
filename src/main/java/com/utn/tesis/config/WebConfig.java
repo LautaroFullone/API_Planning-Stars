3b9a0ed52c836432052a76803b59dd51b9858bc6
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-10-28 16:35:43 -0300

    server allowed origins

--- src/main/java/com/utn/tesis/config/WebConfig.java
+++ src/main/java/com/utn/tesis/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.utn.tesis.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,14 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    //@Value("${client.url}")
-    //private String myAllowedApi;
-
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedMethods("GET","POST","PUT","DELETE")
-                .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://planning-stars.vercel.app");
+                .allowedOrigins("http://localhost:4200", "https://planning-stars.vercel.app", "https://planning-stars.herokuapp.com");
     }
 }
